75edccf1e553076220992460b86ede9c9bf635a9
MOKSH MANRAL <moksh@MOKSHs-MacBook-Air.local>
2025-09-07 18:09:33 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import './App.css'
-import {Routes,Route} from 'react-router-dom'
-import { AboutPage } from './Pages/AboutPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomePage } from './Pages/HomePage';
+import { AboutPage } from './Pages/AboutPage';
 import { ContactPage } from './Pages/ContactPage';
 import { NotFoundPage } from './Pages/NotFoundPage';
 
 function App() {
-  return(
-    <>
-    <Routes>
-      <Route index element={<HomePage/>}/>
-      <Route path='about' element={<AboutPage/>}/>
-      <Route path='contacts' element={<ContactPage/>}/>
-      <Route path='*' element={<NotFoundPage/>}/>
-    </Routes>
-    </>
+  return (
+    <Router basename="/my-site">
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="contacts" element={<ContactPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
   );
 }
 
